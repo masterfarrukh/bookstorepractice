43e67aa4d82f3f69b858c63344a7533eed940905
Farukh Mubashir <masterfarrukh26@gmail.com>
2020-10-06 11:20:05 +0500

    Initial Commit

---
+++ src/main/java/com/bookstore/bookstorepractice/BookstorepracticeApplication.java
@@ -0,0 +1,13 @@
+package com.bookstore.bookstorepractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstorepracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstorepracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookstore/bookstorepractice/BookstorepracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookstore.bookstorepractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstorepracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
